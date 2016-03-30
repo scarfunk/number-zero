61fa43b690acf91e1552e9765499f27eb102e76b
TheBITLINK aka BIT <me@thebitlink.com>
2016-03-30 11:45:46 -0430

    Add browser support. (this probably fixes #2)

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = 0; 
+if(typeof module !== 'undefined')
+{
+  module.exports = 0;
+}
+else if(typeof window !== 'undefined')
+{
+  // Set a global variable for browsers 
+  window.numberZero = 0;
+}
