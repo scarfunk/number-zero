f581a55da45fc90f2bd553b2b97f1ea66a293893
Honza Javorek <aaa@apiary.io>
2016-03-30 22:44:29 +0200

    Fixed linter errors.

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
+/* eslint-disable func-names */
+
 var assert = require('assert');
 
-describe('The object is exported correctly', function() {
-  it('The exported object should be equal to 0', function() {
+
+describe('The object is exported correctly', function () {
+  it('The exported object should be equal to 0', function () {
     assert.equal(require('./index'), 0);
   });
-}); 
+});
