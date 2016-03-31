cc7ccd137b4a94ba0fd56b1298e4340f0058bae3
sukkyu.oh <scarfunk@naver.com>
2016-03-31 21:51:53 +0900

    Revert "Linter"

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
-/* eslint-disable func-names */
-
 var assert = require('assert');
 
-
-describe('The object is exported correctly', function () {
-  it('The exported object should be equal to 0', function () {
+describe('The object is exported correctly', function() {
+  it('The exported object should be equal to 0', function() {
     assert.equal(require('./index'), 0);
   });
-});
+}); 
